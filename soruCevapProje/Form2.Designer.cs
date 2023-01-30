3e514e08acd75ed2bdff419030e00d33bea2f888
metaksoy <aksoylarr@gmail.com>
2023-01-30 11:04:30 +0300

    Add project files.

---
+++ soruCevapProje/Form2.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace soruCevapProje
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.BtnBasla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.lbldogru = new System.Windows.Forms.Label();
+            this.lblsoru = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(99, 117);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(609, 76);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(42, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(218, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(42, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(218, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // BtnBasla
+            // 
+            this.BtnBasla.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnBasla.Location = new System.Drawing.Point(271, 399);
+            this.BtnBasla.Name = "BtnBasla";
+            this.BtnBasla.Size = new System.Drawing.Size(260, 39);
+            this.BtnBasla.TabIndex = 5;
+            this.BtnBasla.Text = "BAŞLA";
+            this.BtnBasla.UseVisualStyleBackColor = true;
+            this.BtnBasla.Click += new System.EventHandler(this.BtnBasla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(602, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Puan :";
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Font = new System.Drawing.Font("Segoe UI Semibold", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblpuan.Location = new System.Drawing.Point(661, 242);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(23, 25);
+            this.lblpuan.TabIndex = 7;
+            this.lblpuan.Text = "0";
+            // 
+            // lbldogru
+            // 
+            this.lbldogru.AutoSize = true;
+            this.lbldogru.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbldogru.Location = new System.Drawing.Point(602, 304);
+            this.lbldogru.Name = "lbldogru";
+            this.lbldogru.Size = new System.Drawing.Size(56, 21);
+            this.lbldogru.TabIndex = 8;
+            this.lbldogru.Text = "Doğru";
+            // 
+            // lblsoru
+            // 
+            this.lblsoru.AutoSize = true;
+            this.lblsoru.Font = new System.Drawing.Font("Segoe UI Semibold", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblsoru.Location = new System.Drawing.Point(70, 116);
+            this.lblsoru.Name = "lblsoru";
+            this.lblsoru.Size = new System.Drawing.Size(23, 25);
+            this.lblsoru.TabIndex = 10;
+            this.lblsoru.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(23, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Soru :";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblsoru);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbldogru);
+            this.Controls.Add(this.lblpuan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnBasla);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.RightToLeftLayout = true;
+            this.Text = "Bilgi Yarışması";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button BtnBasla;
+        private Label label1;
+        private Label lblpuan;
+        private Label lbldogru;
+        private Label lblsoru;
+        private Label label3;
+    }
+}
---
+++ soruCevapProje/Form2.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Media;
+
+
+namespace soruCevapProje
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglan=new SqlConnection("Data Source=DESKTOP-52MVLDJ\\SQLEXPRESS;Initial Catalog=sorularım;Integrated Security=True;TrustServerCertificate=True;");
+        int sayım = 0;  //başla butonuna bastıktan sonra artık sonraki soru yazması için değişken tanımladım
+        
+        int puan = 0;
+
+        void dogrusoru()
+        {
+            if (lbldogru.Text == button1.Text)
+            {
+                button1.BackColor = Color.Green;
+            }else if(lbldogru.Text == button2.Text)
+            {
+                button2.BackColor = Color.Green;
+            }else if(lbldogru.Text== button3.Text) {
+                button3.BackColor = Color.Green;
+            }
+            else if (lbldogru.Text == button4.Text)
+            {
+                button4.BackColor = Color.Green;
+            }
+        }
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\ses.wav");
+            simpleSound.Play();
+        }
+
+        private void BtnBasla_Click(object sender, EventArgs e)
+        {
+            BtnBasla.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            BtnBasla.Text = "Diğer Soruya Geç";
+            sayım++;
+            lblsoru.Text = sayım.ToString();
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\ses.wav");
+            simpleSound.Stop();
+            SoundPlayer simpleSound2 = new SoundPlayer(@"C:\Users\who\Documents\milyoner\sorumusic.wav");
+            simpleSound2.Play();
+
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.White;
+            button3.BackColor = Color.White;
+            button4.BackColor = Color.White;
+
+
+
+
+            if (sayım == 1)
+            {
+                baglan.Open(); //bağlantımızı açtık
+                SqlCommand karar = new SqlCommand("SELECT * FROM soru1 ORDER BY NEWID()", baglan); //Komutumu oluşturuyorum Order by NEWID  diyerek rastgele ID oluşturup seçtiriyorum
+                SqlDataReader okuma = karar.ExecuteReader(); //komutumu yazdırıyorum,Execute Reader ile ilişkilendiriyorum.
+                while (okuma.Read()) //Veri tabanımızdan okudukça
+                {
+                    button1.Text = (okuma["a"].ToString()); //a şıkkını okuttum
+
+                    button2.Text = (okuma["b"].ToString()); //b şıkkımı okuttum
+                    button3.Text = (okuma["c"].ToString());// c şıkkımı okuttum
+                    button4.Text = (okuma["d"].ToString()); // d şıkkımı okuttum
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+
+
+                }
+                baglan.Close();
+            }
+            if (sayım == 2)
+            {
+                baglan.Open();
+                SqlCommand karar = new SqlCommand("SELECT * FROM soru2 ORDER BY NEWID()", baglan);
+                SqlDataReader okuma = karar.ExecuteReader();
+                while (okuma.Read())
+                {
+                    button1.Text = (okuma["a"].ToString());
+                    button2.Text = (okuma["b"].ToString());
+                    button3.Text = (okuma["c"].ToString());// c şıkkımı okuttum
+                    button4.Text = (okuma["d"].ToString()); // d şıkkımı okuttum
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayım == 3)
+            {
+                baglan.Open();
+                SqlCommand karar = new SqlCommand("SELECT * FROM soru3 ORDER BY NEWID()", baglan);
+                SqlDataReader okuma = karar.ExecuteReader();
+                while (okuma.Read())
+                {
+                    button1.Text = (okuma["a"].ToString());
+                    button2.Text = (okuma["b"].ToString());
+                    button3.Text = (okuma["c"].ToString());
+                    button4.Text = (okuma["d"].ToString());
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayım == 4)
+            {
+                baglan.Open();
+                SqlCommand kar = new SqlCommand("SELECT * FROM soru4 ORDER BY NEWID()", baglan);
+                SqlDataReader okuma = kar.ExecuteReader();
+                while (okuma.Read())
+                {
+                    button1.Text = (okuma["a"].ToString());
+                    button2.Text = (okuma["b"].ToString());
+                    button3.Text = (okuma["c"].ToString());
+                    button4.Text = (okuma["d"].ToString());
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+                }
+                baglan.Close();
+
+            }
+            if (sayım == 5)
+            {
+
+                baglan.Open();
+                SqlCommand kar = new SqlCommand("SELECT * FROM soru5 ORDER BY NEWID()", baglan);
+                SqlDataReader okuma = kar.ExecuteReader();
+                while (okuma.Read())
+                {
+                    button1.Text = (okuma["a"].ToString());
+                    button2.Text = (okuma["b"].ToString());
+                    button3.Text = (okuma["c"].ToString());
+                    button4.Text = (okuma["d"].ToString());
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+                    //BtnBasla.Text = "Oyun bitti";
+                    //button1.Enabled = false;
+                    //button2.Enabled = false;
+                    //button3.Enabled = false;
+                    //button4.Enabled = false;
+                }
+
+                baglan.Close();
+
+            }
+            if (sayım == 6)
+            {
+
+                baglan.Open();
+                SqlCommand kar = new SqlCommand("SELECT * FROM soru5 ORDER BY NEWID()", baglan);
+                SqlDataReader okuma = kar.ExecuteReader();
+                while (okuma.Read())
+                {
+
+                    SoundPlayer simpleSound3 = new SoundPlayer(@"C:\Users\who\Documents\milyoner\sorumusic.wav");
+                    simpleSound.Stop();
+                    lblsoru.Text = (sayım-1).ToString();
+                    button1.Text = (okuma["a"].ToString());
+                    button2.Text = (okuma["b"].ToString());
+                    button3.Text = (okuma["c"].ToString());
+                    button4.Text = (okuma["d"].ToString());
+                    textBox1.Text = (okuma["soru"].ToString());
+                    lbldogru.Text = (okuma["dogru"].ToString());
+                    BtnBasla.Text = "Oyun bitti";
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                }
+
+                baglan.Close();
+
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\ses.wav");
+            simpleSound.Play();
+            lblsoru.Text=sayım.ToString();
+            textBox1.Text = "BAŞLAMAK İÇİN BAŞLA BUTONUNA BASINIZ";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            lbldogru.Visible=false;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == lbldogru.Text )
+            {
+                puan = puan + 10;
+                lblpuan.Text=puan.ToString();
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kazanma.wav");
+                simpleSound.Play();
+                button3.BackColor = Color.Red;
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Red;
+                button4.BackColor = Color.Red;
+
+
+
+
+
+
+            }
+            else {
+                puan = puan - 10;
+                lblpuan.Text = puan.ToString();
+                
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kaybetme.wav");
+                simpleSound.Play();
+                button1.BackColor = Color.Red;
+                dogrusoru();
+
+
+
+
+            }
+            
+            BtnBasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(button2.Text == lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text=puan.ToString();
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kazanma.wav");
+                simpleSound.Play();
+                button3.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.Green;
+                button4.BackColor = Color.Red;
+
+
+
+            }
+            else
+            {
+                puan = puan - 10;
+                lblpuan.Text = puan.ToString();
+             
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kaybetme.wav");
+                simpleSound.Play();
+                button2.BackColor = Color.Red;
+                dogrusoru();
+
+
+
+
+            }
+            BtnBasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text = puan.ToString();
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kazanma.wav");
+                simpleSound.Play();
+                button3.BackColor = Color.Green;
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.Red;
+                button4.BackColor = Color.Red;
+
+
+
+            }
+            else
+            {
+                puan = puan - 10;
+                lblpuan.Text = puan.ToString();
+                
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kaybetme.wav");
+                simpleSound.Play();
+                button3.BackColor = Color.Red;
+                dogrusoru();
+
+
+            }
+            BtnBasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text = puan.ToString();
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kazanma.wav");
+                simpleSound.Play();
+                button3.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.Red;
+                button4.BackColor = Color.Green;
+
+            }
+            else
+            {
+                puan = puan - 10;
+                lblpuan.Text = puan.ToString();
+                
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\who\Documents\milyoner\kaybetme.wav");
+                simpleSound.Play();
+                button4.BackColor = Color.Red;
+                dogrusoru();
+
+
+            }
+            BtnBasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
